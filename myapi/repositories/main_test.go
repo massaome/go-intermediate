baa49d014e1370e3ff6befac92b567f881a7bd50
massaome <deepblue.dev@proton.me>
2026-09-23 08:11:30 +0000

    ✨test: 新しいコメントと記事のテストを追加し、DB接続のセットアップを実装
    コメントと記事の取得および挿入に関するテストを新たに追加しました。また、テスト用のデータベース接続とセットアップ、クリーンアップ処理を実装し、テストの実行環境を整えました。これにより、リポジトリの機能を包括的に検証できるようになりました。

---
+++ myapi/repositories/main_test.go
@@ -0,0 +1,78 @@
+package repositories_test
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+)
+
+var testDB *sql.DB
+
+var (
+	dbUser     = "gouser"
+	dbPassword = "gopassword"
+	dbDatabase = "sampledb"
+	dbConn     = fmt.Sprintf("%s:%s@tcp(db:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+)
+
+func connectDB() error {
+	var err error
+	testDB, err = sql.Open("mysql", dbConn)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func setupTestData() error {
+	cmd := exec.Command("mysql", "-h", "db", "-u", "gouser", "sampledb", "--password=gopassword", "-e", "source ./testdata/setupDB.sql")
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func cleanupDB() error {
+	cmd := exec.Command("mysql", "-h", "db", "-u", "gouser", "sampledb", "--password=gopassword", "-e", "source ./testdata/cleanupDB.sql")
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// 全テスト共通の前処理を書く
+func setup() error {
+	if err := connectDB(); err != nil {
+		return err
+	}
+	if err := cleanupDB(); err != nil {
+		fmt.Println("cleanup", err)
+		return err
+	}
+	if err := setupTestData(); err != nil {
+		fmt.Println("setup")
+		return err
+	}
+	return nil
+}
+
+// 前テスト共通の後処理を書く
+func teardown() {
+	cleanupDB()
+	testDB.Close()
+}
+
+func TestMain(m *testing.M) {
+	err := setup()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	m.Run()
+
+	teardown()
+}
